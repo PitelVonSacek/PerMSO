b687f54bbd10570c0cdea9a1c257e5b133100002
Radek Hušek <husek@iuuk.mff.cuni.cz>
2024-09-24 08:39:39 +0200

    improve logging

--- common.py
+++ common.py
@@ -1,12 +1,32 @@
+from os import environ
 import re
 import subprocess
-from os import environ
+from sys import argv
+
+
+def try_log(logfile, fun):
+    if logfile is None: return
+    with open(logfile, "a") as f:
+        print(fun(), file=f)
+
 
 MONA_CMD = environ.get("MONA_CMD", "mona").split()
+MONA_IN_LOG = environ.get("MONA_IN_LOG", None)
+MONA_OUT_LOG = environ.get("MONA_OUT_LOG", None)
 
-def run_mona(mona_desc):
-    return subprocess.check_output(MONA_CMD + [ "-w", "/dev/stdin" ],
+def run_mona(mona_desc, need_automaton=True):
+    cmd = list(MONA_CMD)
+    if need_automaton: cmd += [ "-u", "-w" ]
+    cmd += [ "/dev/stdin" ]
+
+    try_log(MONA_IN_LOG, lambda: f">>> {cmd}\n{mona_desc}\n")
+
+    ret = subprocess.check_output(cmd,
         encoding="utf-8", input=mona_desc, shell=False)
+
+    try_log(MONA_OUT_LOG, lambda: f">>> {cmd}\n{ret}\n")
+
+    return ret
 
 
 def read_up_to(inp, expr):
